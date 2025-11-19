8f0c28423d76c7b2a012a2c1a21ea69470b3ae80
Ana Karla Savini Coelho <70969645+anasavinic@users.noreply.github.com>
2025-11-19 15:31:47 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
 
-# Inicia o MinIO em background
-minio server /data --console-address ":9001" &
-
-# Dá um tempo para o MinIO iniciar
-sleep 5
-
-# Configura o cliente 'mc' dentro do container
+# Configura o cliente mc dentro do container
 mc alias set localminio http://127.0.0.1:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 # Cria o bucket 'havi' se não existir
@@ -16,5 +10,5 @@ mc mb --ignore-existing localminio/havi
 # Define política de leitura pública no bucket 'havi'
 mc anonymous set download localminio/havi
 
-# Mantém o MinIO rodando em foreground
-fg %1
+# Inicia o MinIO em foreground
+exec minio server /data --console-address ":9001"
